9355df8316c8477222391a88ef1b2ad4939f8d31
gabsss1 <202011422@urp.edu.pe>
2023-12-11 17:32:14 -0500

    amo a mi novia <3

--- src/controller/zona.controller.js
+++ src/controller/zona.controller.js
@@ -84,36 +84,14 @@ export const putZona = async (req, res) => {
             features: features.map((feature) => ({
                 type: 'Feature',
                 properties: {},
-                geometry: {
-                    type: feature.geometry.type,
-                    coordinates: feature.geometry.coordinates,
-                },
+                geometry: feature.geometry,
             })),
         };
 
-        const firstFeatureGeometry = features[0].geometry;
-
-        //objetivo: reducir este codigo para que sea mas funcional la programacion en cuanto a  test de tipo geometricos.
-        if (firstFeatureGeometry.type === 'Polygon') {
-            updateZona.geometry = { type: 'Polygon', coordinates: firstFeatureGeometry.coordinates };
-        } else if (firstFeatureGeometry.type === 'Point') {
-            updateZona.geometry = { type: 'Point', coordinates: firstFeatureGeometry.coordinates };
-        } else if (firstFeatureGeometry.type === 'LineString') {
-            updateZona.geometry = { type: 'LineString', coordinates: firstFeatureGeometry.coordinates };
-        } else {
-            if (firstFeatureGeometry.type === 'Circle') {
-                const center = firstFeatureGeometry.center;
-                const radius = firstFeatureGeometry.radius;
-                const circlePolygon = createCirclePolygon(center, radius, 30);
-                updateZona.geometry = { type: 'Polygon', coordinates: circlePolygon };
-            } else if (firstFeatureGeometry.type === 'Rectangle') {
-                const bounds = firstFeatureGeometry.bounds;
-                const rectanglePolygon = createRectanglePolygon(bounds);
-                updateZona.geometry = { type: 'Polygon', coordinates: rectanglePolygon };
-            } else {
-                return res.status(400).json({ message: 'Tipo de geometría no válido' });
-            }
-        }
+        updateZona.geometry = {
+            type: 'GeometryCollection',
+            geometries: features.map(feature => feature.geometry),
+        };
 
         await updateZona.save({ fields: ['name', 'geometry'], hooks: false });
 
@@ -125,8 +103,6 @@ export const putZona = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 };
-
-
 
 // delete zonas
 export const deleteZona = async (req, res) => {
