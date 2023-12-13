381b91bd0589df2e0e4ad32f0c8f106da235b9fc
gabsss1 <202011422@urp.edu.pe>
2023-12-13 13:48:40 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import zonaRoute from './routes/zona.routes.js'
 
 const app = express();
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 
 app.use(zonaRoute)
